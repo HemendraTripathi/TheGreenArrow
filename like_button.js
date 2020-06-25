732316a081c8f9fe6132753c85ea6efdcfebad82
TheGreenArrow <57852366+HemendraTripathi@users.noreply.github.com>
2020-06-25 15:20:52 +0530

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -1,1 +1,2 @@
-const domContainer = document.querySelector('#like_button_container');ReactDOM.render(e(LikeButton), domContainer);
+const domContainer = document.getElementById("root");
+ReactDOM.render(<h2>Hello</h2>,domContainer);
